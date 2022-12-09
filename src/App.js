9009becb69ba86afccb77541110d45877e4560cb
krmp91 <90442900+krmp91@users.noreply.github.com>
2022-12-09 12:33:12 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
