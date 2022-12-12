f38d25ac3c6618efce2fb19354f195e28e1b100e
FIRST_NAME LAST_NAMEYilmaz <MY_NAME@example.comeyatas@hotmail.com>
2022-12-12 13:39:54 +0100

    nav logo ændret lidt homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Component } from "react";
+import  packageInfo from "../components/assets/orangecocktail1.jpg";
 export default function HomePage() {
   return (
+<html >
+<img className="backgroundHome" src={packageInfo}></img>
     <div className="home">
+   
+      
+
       <h1>This is ziropa 2.0</h1>
     </div>
+
+    </html>
+
+
   );
 }
