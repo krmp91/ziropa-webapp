baceba6325111dd96af1022b08037c62748b85a3
krmp91 <90442900+krmp91@users.noreply.github.com>
2022-12-19 19:27:32 +0100

    remaking-productspage

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -11,6 +11,7 @@ export default function AddProducts() {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
+
     image: "",
     createdAt: Timestamp.now().toDate(),
   });
@@ -25,7 +26,7 @@ export default function AddProducts() {
     setFormData({ ...formData, image: e.target.files[0] });
   };
 
-  const handleCreateGroups = () => {
+  const handleCreateProducts = () => {
     if (!formData.title || !formData.description || !formData.image) {
       alert("Please fill all the fields");
       return;
@@ -53,23 +54,24 @@ export default function AddProducts() {
         setFormData({
           title: "",
           description: "",
+
           image: "",
         });
 
         getDownloadURL(uploadImage.snapshot.ref).then((url) => {
-          const groupRef = collection(db, "Groups");
-          addDoc(groupRef, {
+          const titleRef = collection(db, "Produckts");
+          addDoc(titleRef, {
             title: formData.title,
             description: formData.description,
             imageUrl: url,
             createdAt: Timestamp.now().toDate(),
           })
             .then(() => {
-              toast("Product added successfully", { type: "success" });
+              toast("Group created successfully", { type: "success" });
               setProgress(0);
             })
             .catch((error) => {
-              toast("Error adding product", { type: "error" });
+              toast("Error creating group", { type: "error" });
               console.log(error);
             });
         });
@@ -78,101 +80,75 @@ export default function AddProducts() {
   };
 
   return (
-    <Container
-      sx={{
-        maxWidth: "40vh",
-        p: 1,
-      }}
-    >
-      <Typography
-        variant="h4"
-        sx={{
-          p: 2,
+    <div className="form">
+      <h2>Opret gruppe</h2>
+      <label htmlFor="">Gruppens Navn: </label>
+      <input
+        className="form-control"
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={(e) => handleChange(e)}
+      />
+
+      {/*description*/}
+      <label htmlFor="">Beskrivelse: </label>
+      <textarea
+        className="form-control"
+        id="form-control"
+        name="description"
+        value={formData.description}
+        cols=""
+        rows=""
+        onChange={(e) => handleChange(e)}
+      />
+
+      {/* number of peoples in the group */}
+      <TextField
+        className="form-control"
+        type="number"
+        label="Maks antal medlemmer:"
+        name="numberOfPeople"
+        value={formData.numberOfPeople}
+        InputLabelProps={{
+          shrink: true,
         }}
-      >
-        Opret produkt
-      </Typography>
-      <Box
-        Container
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          p: 2,
-        }}
-      >
-        <Box sx={{ py: 1 }}>
-          <TextField
-            sx={{ minWidth: "100%" }}
-            id="outlined-textarea"
-            label="Produktets navn"
-            name="title"
-            multiline
-            defaultValue="Normal"
-            value={formData.title}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={(e) => handleChange(e)}
-          />
-        </Box>
-        {/*description*/}
-        <Box sx={{ py: 1 }}>
-          <TextField
-            sx={{ minWidth: "100%" }}
-            id="outlined-textarea"
-            label="Beskrivelse"
-            name="description"
-            multiline
-            defaultValue="Normal"
-            rows={4}
-            value={formData.description}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={(e) => handleChange(e)}
-          />
-        </Box>
-        {/* image */}
-        <Box sx={{ py: 1 }}>
-          <TextField
-            sx={{ minWidth: "100%" }}
-            label="Indsæt billede"
-            type="file"
-            name="image"
-            accept="image/*"
-            defaultValue="Normal"
-            labelProps={{ style: { color: "white" } }}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={(e) => handleImageChange(e)}
-          />
-        </Box>
-        {progress === 0 ? null : (
-          <div className="progress">
-            <div
-              className="progess-bar progress-bar-striped mt-2"
-              style={{ width: `${progress}%` }}
-            >
-              {`uploading image ${progress}%`}
-            </div>
+        max="50"
+        min="1"
+        onChange={(e) => handleChange(e)}
+      />
+
+      {/* image */}
+      <label htmlFor=""> Billede </label>
+      <input
+        type="file"
+        name="image"
+        accept="image/*"
+        onChange={(e) => handleImageChange(e)}
+      />
+
+      {progress === 0 ? null : (
+        <div className="progress">
+          <div
+            className="progess-bar progress-bar-striped mt-2"
+            style={{ width: `${progress}%` }}
+          >
+            {`uploading image ${progress}%`}
           </div>
-        )}
-      </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Button
-          sx={{
-            border: "var(--border)",
-            color: "var(--addBtnColor)",
-            backgroundColor: "var(--addBtnBGColor)",
-            marginTop: 1,
-          }}
-          className="form-control-btn"
-          onClick={handleCreateGroups}
-        >
-          Tilføj Produkt
-        </Button>
-      </Box>
-    </Container>
+        </div>
+      )}
+      <Button
+        sx={{
+          border: "var(--border)",
+          color: "var(--addBtnColor)",
+          backgroundColor: "var(--addBtnBGColor)",
+          marginTop: 4,
+        }}
+        className="form-control-btn"
+        onClick={handleCreateProducts}
+      >
+        Tilføj Gruppe
+      </Button>
+    </div>
   );
 }
